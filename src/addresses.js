ba8a1b405a91be699891aff4dd238b519451e975
Pawan Singh <pawansinghpks01@gmail.com>
2022-04-16 19:41:07 +0530

    Add files via upload
    Add source files from local

---
+++ src/addresses.js
@@ -0,0 +1,38 @@
+let allAddressRows = [
+	//Web Wallets Addresses
+	{"address":"'CRpmRhUSRcgCxiSCnQpZz6P6rrk1XiPPqD'", "addressSource":"'WebWallet'", "addressName":"CPUChain", "token":"'CPU'"},
+
+	//Vidulum Wallet Addresses
+	{"address":"'sbAmxCuTW8jC1VLesfkvwRTLvEwDQqxa1o'", "addressSource":"'Vidulum'", "addressName":"SCRIV", "token":"'SCRIV'"},
+	{"address":"'B78YM7djG81Td3FPBVnUGwRHbbbvjNgw51'", "addressSource":"'Vidulum'", "addressName":"RITO", "token":"'RITO'"},
+	{"address":"'s1MbfGCRjEVPdFBBVxstkyNSmwiK9u67jVH'", "addressSource":"'Vidulum'", "addressName":"TENT", "token":"'XSG_TENT'"},
+	{"address":"'dgb1qndaa2kww950htvurxqfcjkkpsrwrcf2pjta3a2'", "addressSource":"'Vidulum'", "addressName":"Digibyte", "token":"'DGB'"},
+	
+	//Trust Wallet Addresses
+	{"address":"'TH8sLzLWjp8uRsRQk8igFz3TkMk4nqNHR7'", "addressSource":"'TrustW'", "addressName":"Tron", "token":"'TRX'"},
+	
+	//Binance Wallet Addresses
+	{"address":"'TTgDywjerjxmMzbec1SDzRyfiD7gNdSDm3'", "addressSource":"'Binance'", "addressName":"USDT_TRC20", "token":"'USDT'"},
+	{"address":"'DQ3sGBm1Zarx4upBZMmez6YEEUm8uYvBx1'", "addressSource":"'Binance'", "addressName":"Digibyte", "token":"'DGB'"},
+	{"address":"'DGr8CtSzjpydN8USR7qhqwcaV2YMky5P9z'", "addressSource":"'Binance'", "addressName":"Dogecoin", "token":"'DOGE'"},
+	{"address":"'0x3086dbfe32509cb265cd1c59e92ab1af01d301fa'", "addressSource":"'Binance'", "addressName":"Chiliz", "token":"'CHZ'"},
+
+	//WazirX Wallet Addresses
+	{"address":"'TQsyQoUHa8RyZsYLvFhEV2VadQF9ReLQVH'", "addressSource":"'WazirX'", "addressName":"Tron", "token":"'TRX'"},
+
+	//Klever Wallet Addresses
+	{"address":"'TPAGqyj7HNnGvE96CmnhAhYHoavMVdvGiN'", "addressSource":"'KleverW'", "addressName":"Tron", "token":"'TRX'"},
+	{"address":"'TPAGqyj7HNnGvE96CmnhAhYHoavMVdvGiN'", "addressSource":"'KleverW'", "addressName":"Klever", "token":"'KLV'"},
+
+	//Exodus Wallet Addresses
+	{"address":"'TB2hjzBu5FP4d4DTS4a5bG72E3EJskf8eZ'", "addressSource":"'ExodusW'", "addressName":"Tron", "token":"'TRX'"},
+
+	//CREX24 Wallet Addresses
+	{"address":"'ZRweVcBDQAKXKVwzyFZxU3sSbvqeNuUYKE'", "addressSource":"'Crex24'", "addressName":"ZeroOneCash", "token":"'ZOC'"},
+	{"address":"'sbiMDNt7taG68i5VsU1GiJfDtj5VWiZMuw'", "addressSource":"'Crex24'", "addressName":"Bitcore", "token":"'BTX_BitCore'"},
+	{"address":"'i8Jwpv6rodK7V9nBhDx8wCYgPZBrwnkri8'", "addressSource":"'Crex24'", "addressName":"BITSend", "token":"'BTS_Bitsend'"},
+	{"address":"'CfhnY8kjQaCUsDRioubtuGf1pxUor7yxkz'", "addressSource":"'Crex24'", "addressName":"CPU_Chain", "token":"'CPU'"},
+	{"address":"'DJpHP2JCa8Tu7539pKaofdqEWF2mNHCQhj'", "addressSource":"'Crex24'", "addressName":"Digibyte_DGB", "token":"'DGB'"},
+	{"address":"'MAdYXCGE5G3QeuQpVdc5jiEqoxGQCXz94W'", "addressSource":"'Crex24'", "addressName":"DigitalEuro", "token":"'DigitalEuro'"},
+	{"address":"'FQTxBkRDoyMo9KrM6piuaVvhZu3CoswtDQ'", "addressSource":"'Crex24'", "addressName":"FOXD", "token":"'FOXD'"}
+];
