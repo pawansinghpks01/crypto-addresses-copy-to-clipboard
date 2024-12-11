007eafdbb2f0a3f278caf2de61e2239a14e4ef1e
Pawan Singh <pawansinghpks01@gmail.com>
2024-12-12 04:57:32 +0530

    Update addresses.js

--- src/addresses.js
+++ src/addresses.js
@@ -17,6 +17,7 @@ let allAddressRows = [
 	{"address":"'DGr8CtSzjpydN8USR7qhqwcaV2YMky5P9z'", "addressSource":"'Binance'", "addressName":"Dogecoin", "token":"'DOGE'"},
 	{"address":"'0x3086dbfe32509cb265cd1c59e92ab1af01d301fa'", "addressSource":"'Binance'", "addressName":"Chiliz", "token":"'CHZ'"},
 	{"address":"'RKsJB8p4Aqcx4hxvqxSQdjtU2L9faAeAYk'", "addressSource":"'Binance'", "addressName":"Ravencoin", "token":"'RVN'"},
+	{"address":"'0x6bbfc6fa53cdf7b0ce1f8e4a7665497aadb1b5b2'", "addressSource":"'Binance'", "addressName":"ShibaInu", "token":"'SHIB'"},
 	
 	//WazirX Wallet Addresses
 	{"address":"'TQsyQoUHa8RyZsYLvFhEV2VadQF9ReLQVH'", "addressSource":"'WazirX'", "addressName":"Tron", "token":"'TRX'"},
